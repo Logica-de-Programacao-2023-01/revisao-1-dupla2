c660bb4f613d9a18bee0b89cad9586b261706849
miguelartur <126472309+miguelartur@users.noreply.github.com>
2023-06-06 18:44:48 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -2,5 +2,29 @@ package q2
 
 func AverageLettersPerWord(text string) (float64, error) {
 	// Implemente sua solução aqui
-	return 0, nil
+	package main
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+func AverageLettersPerWord(text string) (float64, error) {
+	if len(text) == 0 || strings.TrimSpace(text) == "" {
+		return 0, errors.New("texto vazio")
+	}
+
+	words := strings.Fields(text)
+	wordCount := len(words)
+	letterCount := 0
+
+	for _, word := range words {
+		letterCount += len(word)
+	}
+
+	average := float64(letterCount) / float64(wordCount)
+	return average, nil
 }
+
+
